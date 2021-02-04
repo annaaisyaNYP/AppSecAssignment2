e88c1b48b20e174afdc7df7090c587f20ad7cb94
annaaisyaNYP <190805H@mymail.nyp.edu.sg>
2021-02-04 18:33:24 +0800

    Fix Missing X-Frame-Options HTTP header Security Flaw

--- AppSecAssignment2/Global.asax.cs
+++ AppSecAssignment2/Global.asax.cs
@@ -17,5 +17,10 @@ namespace AppSecAssignment2
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            HttpContext.Current.Response.AddHeader("X-Frame-Options", "DENY");
+        }
     }
 }
